1a8ece5cdebe83e7f893ec83f518a6e6f5252c06
SamyySwift <samueludofia94@gmail.com>
2024-05-09 19:57:17 +0100

    uploading all files

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import requestRouter from "./src/routes/requestRoute.js";
+import uploadRouter from "./src/routes/uploadRoute.js";
+import cors from "cors";
+import path from "path";
+
+const PORT = 5000;
+const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" })); // Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/upload", uploadRouter);
+app.use("/api", requestRouter);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
